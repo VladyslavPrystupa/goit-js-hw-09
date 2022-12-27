a03da072daecceac64a06d422b707157f8bc8c8a
VladyslavPrystupa <prystupa.vladyslavv@gmail.com>
2022-12-27 21:30:42 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector('[data-start]')
+const stoptBtn = document.querySelector('[data-stop]')
+const body = document.querySelector('body')
+let timerId = null
+// console.log(startBtn)
+
+
+startBtn.addEventListener("click", start)
+stoptBtn.addEventListener("click", stop)
+
+function start() {
+    timerId = setInterval(()=>{  
+    const color = getRandomHexColor();
+    body.style.backgroundColor = color
+}, 1000 )
+    if(timerId){
+    return startBtn.disabled = true
+    }
+}
+
+function stop(){
+ clearInterval(timerId);
+ startBtn.disabled = false
+} 
+
+
+
+
+
 
